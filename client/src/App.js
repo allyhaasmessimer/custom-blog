3d027af4651342a8c0de576c84e36262d7439894
allyhaasmessimer <allyhaas@gmail.com>
2024-06-20 09:52:24 -0500

    react app created

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <main>
+                <header>
+                    <a href="" className="logo">
+                        LieselSchmidt
+                    </a>
+                    <nav>
+                        <a href="">Login</a>
+                        <a href="">Register</a>
+                    </nav>
+                </header>
+            </main>
+        </div>
+    );
+}
+
+export default App;
