76c5af3259777b35a57697f58d60d4615c2d9e16
allyhaasmessimer <allyhaas@gmail.com>
2024-06-25 14:52:20 -0500

    blog ready to deploy

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,35 +1,9 @@
 import { useState } from "react";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { Navigate } from "react-router-dom";
+import Editor from "../Editor";
 
-const modules = {
-    toolbar: [
-        [{ header: [1, 2, false] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [
-            { list: "ordered" },
-            { list: "bullet" },
-            { indent: "-1" },
-            { indent: "+1" },
-        ],
-        ["link", "image"],
-        ["clean"],
-    ],
-};
-const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-];
 export default function CreatePost() {
     const [title, setTitle] = useState("");
     const [summary, setSummary] = useState("");
@@ -78,12 +52,7 @@ export default function CreatePost() {
                 // value={files}
                 onChange={(ev) => setFiles(ev.target.files)}
             />
-            <ReactQuill
-                value={content}
-                onChange={(newValue) => setContent(newValue)}
-                modules={modules}
-                formats={formats}
-            />
+            <Editor value={content} onChange={setContent} />
             <button style={{ marginTop: "5px" }}>Create post</button>
         </form>
     );
