c1eb3b03458f5d9c34c162db4a7173527f6cf3cf
allyhaasmessimer <allyhaas@gmail.com>
2024-07-15 08:38:18 -0500

    styled blog post list page

--- client/src/Post.js
+++ client/src/Post.js
@@ -11,23 +11,25 @@ export default function Post({
     author,
 }) {
     return (
-        <div className="post">
-            <div className="image">
-                <Link to={`/post/${_id}`}>
-                    <img src={cover} alt={title} />
-                </Link>
-            </div>
-            <div className="texts">
-                <Link to={`/post/${_id}`}>
-                    <h2>{title}</h2>
-                </Link>
-                <p className="info">
-                    <span className="author">{author?.username}</span>
-                    <time dateTime="">
-                        {format(new Date(createdAt), "MMM d, yyyy")}
-                    </time>
-                </p>
-                <p className="summary">{summary}</p>
+        <div className="post_page_box">
+            <div className="post">
+                <div className="image">
+                    <Link to={`/post/${_id}`}>
+                        <img src={cover} alt={title} />
+                    </Link>
+                </div>
+                <div className="texts">
+                    <Link to={`/post/${_id}`}>
+                        <h2>{title}</h2>
+                    </Link>
+                    <p className="info">
+                        <span className="author">{author?.username}</span>
+                        <time dateTime="">
+                            {format(new Date(createdAt), "MMM d, yyyy")}
+                        </time>
+                    </p>
+                    <p className="summary">{summary}</p>
+                </div>
             </div>
         </div>
     );
